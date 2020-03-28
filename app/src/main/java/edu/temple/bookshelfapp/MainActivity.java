9c4e069ae416e575dfc8a356b5cf76d5229d203a
Parsa Temple Owl <tug81042@temple.edu>
2020-03-28 19:37:04 -0400

    added 12 books with their titles and authors to the hash map in the main activity

--- app/src/main/java/edu/temple/bookshelfapp/MainActivity.java
+++ app/src/main/java/edu/temple/bookshelfapp/MainActivity.java
@@ -30,16 +30,18 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
             bookDetailsFragment = new BookDetailsFragment();
             fragmentManager.beginTransaction().replace(R.id.container_2, bookDetailsFragment).commit();
         }
-        hashMap.put("Book 1", "Author 1");
-        hashMap.put("Book 2", "Author 2");
-        hashMap.put("Book 3", "Author 3");
-        hashMap.put("Book 4", "Author 4");
-        hashMap.put("Book 5", "Author 5");
-        hashMap.put("Book 6", "Author 6");
-        hashMap.put("Book 7", "Author 7");
-        hashMap.put("Book 8", "Author 8");
-        hashMap.put("Book 9", "Author 9");
-        hashMap.put("Book 10", "Author 10");
+        hashMap.put("HTML & CSS", "Jon Duckett");
+        hashMap.put("JavaScript & JQuery", "Jon Duckett");
+        hashMap.put("Pro Git: everything you need to know about Git", "Scott Chacom");
+        hashMap.put("The Hunger Games", "Suzanne Collins");
+        hashMap.put("Catching Fire", "Suzanne Collins");
+        hashMap.put("Mockingjay", "Suzanne Collins");
+        hashMap.put("Twilight", "Stephenie Meyer");
+        hashMap.put("Peeps", "Scott Westerfeld");
+        hashMap.put("Python for Everybody", "Charles Severance");
+        hashMap.put("Git In Practice", "Mike McQuaid");
+        hashMap.put("Pragmatic Guide to Git", "Travis Swicegood");
+        hashMap.put("Mastering Sublime Text", "Dan Peleg");
         hashMapArrayList.add(hashMap);
        bookListFragment.newInstance(hashMapArrayList);
     }
@@ -57,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
     public void bookSelected(int index) {
         book = hashMapArrayList.get(index);
         if (singleContainer) {
-            fragmentManager.beginTransaction().replace(R.id.container_1, bookDetailsFragment.newInstance().addToBackStack(null).commit();
+            fragmentManager.beginTransaction().replace(R.id.container_1, bookDetailsFragment.newInstance(book)).addToBackStack(null).commit();
         }
         else {
             bookDetailsFragment.displayBook(book);
