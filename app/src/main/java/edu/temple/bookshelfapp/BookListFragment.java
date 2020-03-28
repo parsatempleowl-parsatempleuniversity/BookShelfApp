4c3856662e5af235478b120b0979e0ecb3180127
Parsa Temple Owl <tug81042@temple.edu>
2020-03-27 22:06:28 -0400

    created a blank fragment and called it book list fragment

---
+++ app/src/main/java/edu/temple/bookshelfapp/BookListFragment.java
@@ -0,0 +1,22 @@
+package edu.temple.bookshelfapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BookListFragment extends Fragment {
+
+    public BookListFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_book_list, container, false);
+    }
+}
